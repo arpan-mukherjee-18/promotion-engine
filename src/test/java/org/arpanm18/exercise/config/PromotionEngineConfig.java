ef830a37545af564e7b4921a3ac300712184ca63
arpan-mukherjee-18 <arpan.mukherjee.1810@gmail.com>
2022-12-17 16:18:13 +0000

    Scenario A Test Case

--- src/test/java/org/arpanm18/exercise/config/PromotionEngineConfig.java
+++ src/test/java/org/arpanm18/exercise/config/PromotionEngineConfig.java
@@ -1,2 +1,9 @@
-package org.arpanm18.exercise.context;public class PromotionEngineTestContext {
+package org.arpanm18.exercise.config;
+
+import org.arpanm18.exercise.service.PromotionEngine;
+import org.arpanm18.exercise.service.PromotionEngineImpl;
+
+public class PromotionEngineConfig {
+
+    private final PromotionEngine promotionEngine = new PromotionEngineImpl();
 }
